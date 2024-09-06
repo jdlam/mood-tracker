f88ac27de287df8dd97ba672007fd79e6c4b7eb1
Jon Lam <jonlam92@gmail.com>
2024-09-05 22:32:11 -0400

    add font awesome

--- src/components/MoodTracker.vue
+++ src/components/MoodTracker.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+interface MoodObject {
+  value: string
+  icon: string
+}
+
 // TODO: move this out to its own file later with harder typing
 const moods: string[] = ['Happy', 'Content', 'Sad']
+const moodObjects: MoodObject[] = [
+  {
+    value: 'Happy',
+    icon: 'face-smile'
+  },
+  {
+    value: 'Content',
+    icon: 'face-meh'
+  },
+  {
+    value: 'Sad',
+    icon: 'face-frown'
+  }
+]
 
 const track = (mood: string) => {
   console.log('mood is', mood)
@@ -8,11 +29,41 @@ const track = (mood: string) => {
 </script>
 
 <template>
-  <div class="container">
-    <div v-for="mood in moods" v-bind:key="mood" @click="track(mood)" class="button">
-      {{ mood }}
+  <div class="mood-container">
+    <div
+      v-for="mood in moodObjects"
+      v-bind:key="mood.value"
+      @click="track(mood.value)"
+      class="button"
+    >
+      <FontAwesomeIcon :icon="['far', mood.icon]" />
+      <p>{{ mood.value }}</p>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.mood-container {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.button {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  align-items: center;
+}
+
+.button:hover {
+  border: 1px white;
+}
+
+.button > svg {
+  height: 40px;
+  width: 40px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,23 @@ import router from './router'
 
 const app = createApp(App)
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faFaceFrown, faFaceMeh, faFaceSmile } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFaceFrown as farFaceFrown,
+  faFaceMeh as farFaceMeh,
+  faFaceSmile as farFaceSmile
+} from '@fortawesome/free-regular-svg-icons'
+// import { faFaceMeh } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faFaceFrown, faFaceMeh, faFaceSmile, farFaceFrown, farFaceMeh, farFaceSmile)
+
+/* add icons to the library */
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 app.use(createPinia())
 app.use(router)
 
